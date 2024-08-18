e6f117716a55356d8d6d36300e592eab137b3f42
JW <jacekwoln@gmail.com>
2024-08-18 13:34:15 +0200

    Add temel to modules

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('readme.md', 'r', encoding='utf-8') as f:
 
 setup(
     name="temel",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     py_modules=['temel'],
     install_requires=[
